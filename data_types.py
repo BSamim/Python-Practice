9db62bb2e07e3c55447859744ee68ef3598d3d10
BSamim <bsamimzubair@gmail.com>
2023-04-13 07:16:38 +0500

    Data Types in python

---
+++ data_types.py
@@ -0,0 +1,13 @@
+integer_number = 123
+float_number = 1.23
+
+new_number = integer_number + float_number
+
+# display new value and resulting data type
+print("Value:",new_number)
+print("Data Type:",type(new_number))
+
+
+string = 'this is a string'
+print("Value:",string)
+print("Data Type:",type(string))
