4aba22d29446670dd98a64cffc7e6a7220278e4c
BSamim <bsamimzubair@gmail.com>
2023-04-15 06:35:02 +0500

    user input

---
+++ user_input.py
@@ -0,0 +1,12 @@
+name = input('What is your name?: ')
+age = input('What is your age?: ')
+print('Hello ' + name + '! You are ' + age + ' years old.')
+
+num1 = input('Enter a digit: ')
+num2 = input('Enter a second number:')
+answer = float(num1)+float(num2)
+print(answer)
+
+first_Name = input('What is your first name?: ')
+distance = input('How much far you live from devsinc?(in km): ')
+print(f'Hello {first_Name} welcome to my program you are {distance} km or {float(distance)/1.609} mile away from devsinc')
